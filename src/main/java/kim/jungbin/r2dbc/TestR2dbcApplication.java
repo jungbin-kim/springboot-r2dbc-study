0bfab944e992abd9f3d1ad12e473accda5eab65b
Jungbin Kim <mail@jungbin.kim>
2021-04-05 22:03:03 +0900

    Init R2dbc Mysql Project

---
+++ src/main/java/kim/jungbin/r2dbc/TestR2dbcApplication.java
@@ -0,0 +1,13 @@
+package kim.jungbin.r2dbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestR2dbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestR2dbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kim/jungbin/r2dbc/TestR2dbcApplicationTests.java
@@ -0,0 +1,13 @@
+package kim.jungbin.r2dbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestR2dbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
